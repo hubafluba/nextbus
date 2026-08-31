652bd7b6349ca8c3cf8860ef261e964fec3cebd2
Mustafa Farhat <mustafa@Mustafas-MacBook-Air-3.local>
2026-08-31 22:18:44 +1200

    Turned the script into a function, and added parameters of stop id and limit which defaults to 3, limit showing how many bus departure times it should print out.

--- exploring.py
+++ exploring.py
@@ -5,35 +5,43 @@ from datetime import datetime
 
 load_dotenv()
 key = os.environ["AT_API_KEY"]
-now = datetime.now()
 
-stop_id = "7394-7baa4c89"
-url = f"https://api.at.govt.nz/gtfs/v3/stops/{stop_id}/stoptrips"
+def get_departures(stop_id, limit=3):
+    now = datetime.now()
 
-params = {
-    "filter[date]": now.strftime("%Y-%m-%d"),
-    "filter[start_hour]": now.hour
-}
+    url = f"https://api.at.govt.nz/gtfs/v3/stops/{stop_id}/stoptrips"
+    params = {
+        "filter[date]": now.strftime("%Y-%m-%d"),
+        "filter[start_hour]": now.hour
+    }
+    headers = {"Ocp-Apim-Subscription-Key": key}
+    r = requests.get(url, params=params, headers=headers)
 
-headers = {"Ocp-Apim-Subscription-Key": key}
+    upcoming = []
+    for item in r.json()["data"]:
+        attributes = item["attributes"]
+        dep = datetime.strptime(attributes["departure_time"], "%H:%M:%S").time()
+        if dep >= now.time():
+            upcoming.append(attributes)
 
-r = requests.get(url, params=params, headers=headers)
-upcoming = []
-for item in r.json()["data"]:
-    attributes = item["attributes"]
-    dep = datetime.strptime(attributes["departure_time"], "%H:%M:%S").time()
-    if dep >= now.time():
-        upcoming.append(attributes)
+    route_ids = {a["route_id"] for a in upcoming[:limit]}
+    names = {}
+    for rid in route_ids:
+        r2 = requests.get(f"https://api.at.govt.nz/gtfs/v3/routes/{rid}", headers=headers)
+        names[rid] = r2.json()["data"]["attributes"]["route_short_name"]
 
-route_ids = {a["route_id"] for a in upcoming[:3]}
-names = {}
-for rid in route_ids:
-    r2 = requests.get(f"https://api.at.govt.nz/gtfs/v3/routes/{rid}", headers=headers)
-    names[rid] = r2.json()["data"]["attributes"]["route_short_name"]
+    results = []
+    for attributes in upcoming[:limit]:
+        dep = datetime.strptime(attributes["departure_time"], "%H:%M:%S").time()
+        dep_dt = datetime.combine(now.date(), dep)
+        mins = int((dep_dt - now).total_seconds() // 60)
+        results.append({
+            "route": names[attributes["route_id"]],
+            "minutes": mins,
+            "destination": attributes["stop_headsign"],
+        })
+    return results
 
-for attributes in upcoming[:3]:
-    dep = datetime.strptime(attributes["departure_time"], "%H:%M:%S").time()
-    dep_dt = datetime.combine(now.date(), dep)
-    mins = int((dep_dt - now).total_seconds() // 60)
-    name = names[attributes["route_id"]]
-    print(f"{name}  {mins} minutes away")
+if __name__ == "__main__":
+    for d in get_departures("7394-7baa4c89", 3):
+        print(f"{d['route']}  {d['minutes']} minutes away")
